489c6f646cf2bb7be8f4dc1826d47bd8d567e16f
Dmitrii Grebeniuk <dmtrgrbnk@gmail.com>
2023-03-12 20:24:49 +0300

    new test

---
+++ src/test/java/homework.java
@@ -0,0 +1,30 @@
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byLinkText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class homework {
+
+   @BeforeAll
+   static void beforeAll() {
+      Configuration.browserSize = "1440x812";
+   }
+
+   @Test
+   void SoftAssertions() {
+
+      open("https://github.com/selenide/selenide");
+
+      $("#wiki-tab").click();
+      $(".js-wiki-more-pages-link").click();
+      $(byLinkText("SoftAssertions")).click();
+      $(".markdown-body").shouldHave(text("Using JUnit5 extend test class"));
+
+   }
+}
